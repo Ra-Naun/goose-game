87f9979f9cb696d71547122d20deb6990a6877f4
Ra-Naun <dmitriy_slavskiy@mail.ru>
2025-09-11 09:23:00 +0300

    t-9/2025-09-11/production-v2.2

--- website/app/src/config/env.ts
+++ website/app/src/config/env.ts
@@ -1,8 +1,9 @@
-export const USER_WEBSOCKET_URL = import.meta.env.VITE_USER_WEBSOCKET_URL || "ws://localhost";
+export const USER_WEBSOCKET_URL = import.meta.env.VITE_USER_WEBSOCKET_URL || "wss://localhost/api";
 export const USER_WEBSOCKETS_NAMESPACE = import.meta.env.VITE_USER_WEBSOCKETS_NAMESPACE || "";
-export const USER_WEBSOCKET_HANDSHAKE_PATH = import.meta.env.VITE_USER_WEBSOCKET_HANDSHAKE_PATH || "/socket.io";
+export const USER_WEBSOCKET_HANDSHAKE_PATH =
+  import.meta.env.VITE_USER_WEBSOCKET_HANDSHAKE_PATH || "/api/user/socket.io";
 
-export const TAP_GOOSE_WEBSOCKET_URL = import.meta.env.VITE_TAP_GOOSE_WEBSOCKET_URL || "ws://localhost";
+export const TAP_GOOSE_WEBSOCKET_URL = import.meta.env.VITE_TAP_GOOSE_WEBSOCKET_URL || "wss://localhost/api";
 export const TAP_GOOSE_WEBSOCKETS_NAMESPACE = import.meta.env.VITE_TAP_GOOSE_WEBSOCKETS_NAMESPACE || "";
 export const TAP_GOOSE_WEBSOCKET_HANDSHAKE_PATH =
-  import.meta.env.VITE_TAP_GOOSE_WEBSOCKET_HANDSHAKE_PATH || "/socket.io";
+  import.meta.env.VITE_TAP_GOOSE_WEBSOCKET_HANDSHAKE_PATH || "/api/tap-goose-game/socket.io";
